d8335c722504b7b4ad55a21478191cd666975805
Renato Lucio de Mendonça <neo_rlm@hotmail.com>
2023-07-19 17:30:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
-const express = require("express");
-const { scrapeLogic } = require("./scrapeLogic");
-const app = express();
+const puppeteer = require("puppeteer");
+require("dotenv").config();
 
-const PORT = process.env.PORT || 4000;
+const scrapeLogic = async (res) => {
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
+  try {
+    const page = await browser.newPage();
 
-app.get("/scrape", (req, res) => {
-  scrapeLogic(res);
-});
+    await page.goto("https://developer.chrome.com/");
 
-app.get("/", (req, res) => {
-  res.send("Render Puppeteer server is up and running!");
-});
+    // Set screen size
+    await page.setViewport({ width: 1080, height: 1024 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+    // Type into search box
+    await page.type(".search-box__input", "automate beyond recorder");
+
+    // Wait and click on first result
+    const searchResultSelector = ".search-box__link";
+    await page.waitForSelector(searchResultSelector);
+    await page.click(searchResultSelector);
+
+    // Locate the full title with a unique string
+    const textSelector = await page.waitForSelector(
+      "text/Customize and automate"
+    );
+    const fullTitle = await textSelector.evaluate((el) => el.textContent);
+
+    // Print the full title
+    const logStatement = `The title of this blog post is ${fullTitle}`;
+    console.log(logStatement);
+    res.send(logStatement);
+  } catch (e) {
+    console.error(e);
+    res.send(`Something went wrong while running Puppeteer: ${e}`);
+  } finally {
+    await browser.close();
+  }
+};
+
+module.exports = { scrapeLogic };
