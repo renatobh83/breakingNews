faa82b171522450d20e91cb2b465277227eb64e1
Renato Lucio de Mendonça <neo_rlm@hotmail.com>
2023-07-19 17:30:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,17 @@
-const puppeteer = require("puppeteer");
-require("dotenv").config();
+const express = require("express");
+const { scrapeLogic } = require("./scrapeLogic");
+const app = express();
 
-const scrapeLogic = async (res) => {
-  const browser = await puppeteer.launch({
-    args: [
-      "--disable-setuid-sandbox",
-      "--no-sandbox",
-      "--single-process",
-      "--no-zygote",
-    ],
-    executablePath:
-      process.env.NODE_ENV === "production"
-        ? process.env.PUPPETEER_EXECUTABLE_PATH
-        : puppeteer.executablePath(),
-  });
-  try {
-    const page = await browser.newPage();
+const PORT = process.env.PORT || 4000;
 
-    await page.goto("https://developer.chrome.com/");
+app.get("/scrape", (req, res) => {
+  scrapeLogic(res);
+});
 
-    // Set screen size
-    await page.setViewport({ width: 1080, height: 1024 });
+app.get("/", (req, res) => {
+  res.send("Render Puppeteer server is up and running!");
+});
 
-    // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder");
-
-    // Wait and click on first result
-    const searchResultSelector = ".search-box__link";
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
-
-    // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector(
-      "text/Customize and automate"
-    );
-    const fullTitle = await textSelector.evaluate((el) => el.textContent);
-
-    // Print the full title
-    const logStatement = `The title of this blog post is ${fullTitle}`;
-    console.log(logStatement);
-    res.send(logStatement);
-  } catch (e) {
-    console.error(e);
-    res.send(`Something went wrong while running Puppeteer: ${e}`);
-  } finally {
-    await browser.close();
-  }
-};
-
-module.exports = { scrapeLogic };
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
