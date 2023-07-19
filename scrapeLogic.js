b52393b9c74020d46102f30f5af0b41db3516da9
Renato Lucio de Mendonça <neo_rlm@hotmail.com>
2023-07-19 17:30:33 -0300

    Update scrapeLogic.js

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -2,31 +2,43 @@ const puppeteer = require("puppeteer");
 require("dotenv").config();
 
 const scrapeLogic = async (res) => {
-
   const browser = await puppeteer.launch({
-
     args: [
       "--disable-setuid-sandbox",
       "--no-sandbox",
       "--single-process",
       "--no-zygote",
     ],
-    headless: 'new',  
     executablePath:
       process.env.NODE_ENV === "production"
         ? process.env.PUPPETEER_EXECUTABLE_PATH
         : puppeteer.executablePath(),
   });
   try {
-    
     const page = await browser.newPage();
 
-    await page.goto("https://pptr.dev/guides/docker",{waitUntil: "domcontentloaded"});
+    await page.goto("https://developer.chrome.com/");
 
-    const fullTitle = await page.title()
+    // Set screen size
+    await page.setViewport({ width: 1080, height: 1024 });
+
+    // Type into search box
+    await page.type(".search-box__input", "automate beyond recorder");
+
+    // Wait and click on first result
+    const searchResultSelector = ".search-box__link";
+    await page.waitForSelector(searchResultSelector);
+    await page.click(searchResultSelector);
+
+    // Locate the full title with a unique string
+    const textSelector = await page.waitForSelector(
+      "text/Customize and automate"
+    );
+    const fullTitle = await textSelector.evaluate((el) => el.textContent);
+
     // Print the full title
     const logStatement = `The title of this blog post is ${fullTitle}`;
-    
+    console.log(logStatement);
     res.send(logStatement);
   } catch (e) {
     console.error(e);
