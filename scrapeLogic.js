1e5d34e22b44090213e632f0f9450941f1b43035
Renato Lucio de Mendonça <neo_rlm@hotmail.com>
2023-07-19 17:40:38 -0300

    Update scrapeLogic.js

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -17,7 +17,7 @@ const scrapeLogic = async (res) => {
   try {
     const page = await browser.newPage();
 
-    await page.goto("https://developer.chrome.com/");
+    await page.goto(res.req.query.url);
 
     // Set screen size
     await page.setViewport({ width: 1080, height: 1024 });
