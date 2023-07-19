0391bfb734f208f16ea084c15ece20ef9e4252dd
Renato Lucio de Mendonça <neo_rlm@hotmail.com>
2023-07-19 17:13:41 -0300

    Add files via upload

---
+++ scrapeLogic.js
@@ -0,0 +1,42 @@
+const puppeteer = require("puppeteer");
+require("dotenv").config();
+
+const scrapeLogic = async (res) => {
+
+  const browser = await puppeteer.launch({
+
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    headless: 'new',  
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
+  try {
+    
+    const page = await browser.newPage();
+
+    await page.goto(res.req.query.url);
+
+    // Set screen size
+    await page.setViewport({ width: 1080, height: 1024 });
+
+    const fullTitle = await page.title()
+    // Print the full title
+    const logStatement = `The title of this blog post is ${fullTitle}`;
+    
+    res.send(logStatement);
+  } catch (e) {
+    console.error(e);
+    res.send(`Something went wrong while running Puppeteer: ${e}`);
+  } finally {
+    await browser.close();
+  }
+};
+
+module.exports = { scrapeLogic };
