4625aba06e55bc87a38a164875da1d5c27048c2f
Renato Lucio de Mendonça <neo_rlm@hotmail.com>
2023-07-20 14:30:23 -0300

    Update scrapeLogic.js

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -82,7 +82,20 @@ const scrapeLogic = async (res) => {
        noticias = noticias.concat(noticiaJornal)
     }
       }
+
+  res.set('Access-Control-Allow-Origin', '*');
+
+  if (req.method === 'OPTIONS') {
+    // Send response to OPTIONS requests
+    res.set('Access-Control-Allow-Methods', 'GET');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Max-Age', '3600');
+    res.status(204).send('');
+  } else {
     res.send(noticias);
+  }
+
+
   } catch (e) {
 
     res.send(`Something went wrong while running Puppeteer: ${e}`);
